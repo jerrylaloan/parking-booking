1e10145932c0b4e7801ea539047fe75556e67e66
Jerry Laloan <jerrylaloan@hotmail.com>
2022-03-28 23:55:40 +0700

    Try mock implementation

--- server/src/tests/Feature/BaysTest.php
+++ server/src/tests/Feature/BaysTest.php
@@ -5,20 +5,44 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Mockery\MockInterface;
+use App\Services\BayService;
 
 class BaysTest extends TestCase
 {
-    /**
-     * Testing invocation for list of bays resource endpoint.
-     *
-     * @return array
-     */
-    public function test_should_return_200_response_for_successful_api_invocation()
-    {
-        $response = $this->get('/api/bays');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
-    }
+	public function setUp(): void
+	{
+		parent::setUp();
+
+		$mock = $this->mock(BayService::class, function (MockInterface $mock) {
+    		$mock->shouldReceive('getAll')->with('all')->andReturn([
+				[
+            		'id' => 1,
+            		'location' => 'location 1',
+            		'available' => true
+				], 
+				[
+            		'id' => 2,
+            		'location' => 'location 2',
+            		'available' => true
+				], 
+				[
+            		'id' => 3,
+            		'location' => 'location 3',
+            		'available' => true
+				]
+			]);
+		});
+	}
+
+ 	public function test_should_return_200_response_for_successful_api_invocation()
+ 	{
+ 	    $response = $this->get('/api/bays');
+
+ 	    $response->assertStatus(200);
+ 	}
 
     public function test_should_return_3_bays_in_the_response_data()
     {
