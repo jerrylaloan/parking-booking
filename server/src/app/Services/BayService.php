52706db6dcd9b1c5c907ff981775a81dee765a26
Jerry Laloan <jerrylaloan@hotmail.com>
2022-03-31 01:59:57 +0700

    Introduce php doc in BayService

--- server/src/app/Services/BayService.php
+++ server/src/app/Services/BayService.php
@@ -5,12 +5,25 @@ namespace App\Services;
 use App\Models\Bay;
 
 class BayService {
+
+    /**
+     * Fetch all bay
+     * 
+     * @param  string $status - enum of status ('all'|'available'|'not_available')
+     * @return Bay[] - list of bay
+     */
     public function getAll($status) {
         if ($status == 'all') return Bay::all();
 
         return Bay::where('available', $status == 'available' ? true : false)->get();
     }
 
+    /**
+     * Check bay availability by id
+     * 
+     * @param  string $id - bay id
+     * @return boolean - value of available attributes
+     */
     public function bayIsAvailable($id){
         return (Bay::find($id))->available;
     }
