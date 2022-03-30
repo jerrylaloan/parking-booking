6442ee3810d2873ee2a54d3c5306cea48cdeca66
Jerry Laloan <jerrylaloan@hotmail.com>
2022-03-31 01:16:07 +0700

    Added models & migration for transactions table

---
+++ server/src/database/migrations/2022_03_30_171015_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('booking_id')->constrained('bookings');
+            $table->float('duration')->nullable(false);
+            $table->integer('amount')->nullable(false);
+            $table->enum('payment_method', ['cash'])->nullable(false);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
